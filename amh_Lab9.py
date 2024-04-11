49572844182994d9bf5ec757cdaf7088fa6d4cc2
Shane Wooden <shanewooden77@gmail.com>
2024-04-11 13:30:05 -0400

    second

--- amh_Lab9.py
+++ amh_Lab9.py
@@ -1,4 +1,17 @@
 #Alexander Heard
+#hi
+def decode(encoded_password):
+    decoded_password=""
+    for character in encoded_password:
+        if int(character)>=3:
+            value_to_add=int(character)-3
+            value_to_add=str(value_to_add)
+            decoded_password+=value_to_add
+        elif int(character)<3:
+            changed_value=int(character)+10
+            value_to_add=changed_value-3
+            decoded_password+=str(value_to_add)
+    return decoded_password
 
 def encode(password):
     new_password = ''
